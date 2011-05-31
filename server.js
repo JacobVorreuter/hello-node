a4ade3bc47be4f8a78f900c5bbd1c71783fd25fe
Jacob Vorreuter <jake@heroku.com>
2011-05-31 16:40:01 -0700

    asdf

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var http = require('http'),
 var port = parseInt(process.env['PORT']);
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  req.onBody = function(chunk) {
-    res.write(chunk);
-  } 
-  req.onBodyComplete = function() {
-    res.end("body complete");
-  }
+  console.log("req start");
+  req.on('data', function(chunk) {
+    console.log("body recvd");
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(chunk + '\n');
+  });
+  req.on('end', function() {
+    res.end("body complete\n");
+  });
 }).listen(port, "0.0.0.0");
 console.log('Server bound to port ' + port.toString());
