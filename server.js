3b4dde7517ace66fbb2b64c25ce70264ab728512
Jacob Vorreuter <jake@heroku.com>
2011-05-31 16:45:01 -0700

    asdf

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var port = parseInt(process.env['PORT']);
 
 http.createServer(function (req, res) {
   console.log("req start");
-  req.on('data', function(chunk) {
+ /* req.on('data', function(chunk) {
     console.log("body recvd");
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(chunk + '\n');
   });
   req.on('end', function() {
     res.end("body complete\n");
-  });
+  });*/
 }).listen(port, "0.0.0.0");
 console.log('Server bound to port ' + port.toString());
